d0fa07586574b91e9906e8c2214f977ea8e602c0
ArberMuca <36955742+ArberMuca@users.noreply.github.com>
2018-06-06 17:54:00 +0200

    Kommentare
    Ich habe, was jede Zeile macht, kommentiert.

--- GAMMA_RX8.ino
+++ GAMMA_RX8.ino
@@ -1,27 +1,6 @@
-/* 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  RF SOLUTIONS SAMPLE GAMMA RECEIVER MODE 8 CHANNELS
-  WEB SITE : http://www.rfsolutions.co.uk
- 
-  Address:
-  	William Alexander House
-	William Way, Burgess Hill, West Sussex
-	RH15 9AG
-	+44 (0)1444 227900
-	(01444) 227900 
- 
-  Code by: AB
-  Date:    11/05/2015
-  
-  SAMPLE CODE
-   When a channel is pressed on a GAMMA TX the channel is displayed on a terminal program 
-   baud rate 19200
-     
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*/
 
-// DEFINE CHANNELS AND OPTION LINKS 
-#define  OP1 2  // All channels are inputs on the Arduino when configured as a RECEIVER
+// definition von alle Channels
+#define  OP1 2  // Alle Channels sind INPUT, wenn sie als Reciever configuriert sind
 #define  OP2 3
 #define  OP3 4
 #define  OP4 5
@@ -30,14 +9,13 @@
 #define  OP7 8
 #define  OP8 9
 
-  
-  int StateChange = 0;           // state change flag value 
-  
-  byte Inputs18 = B00000000;     // Clear all inputs flags before use  
-  
+
+int StateChange = 0;           // Flag Wert
+
+byte Inputs18 = B00000000;     // lösche alle Flag vor der Ausführung
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, INPUT);            // all outputs to control BRAVO in TX Mode 
+  pinMode(2, INPUT);            // INPUTS
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
@@ -45,137 +23,137 @@ void setup() {
   pinMode(7, INPUT);
   pinMode(8, INPUT);
   pinMode(9, INPUT);
-  
-  Serial.begin(19200);         // enable serial to see what button is pressed on a terminal program
+
+  Serial.begin(19200);         // Serial einschalten auf 19200 baut
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-// Test output 1 
+  // OUT 1 testen
   StateChange = bitRead(Inputs18, 0);  // Read last known state of output 1
-  
-if ((digitalRead(OP1)) == HIGH) {      
-    if (StateChange == 0) {              // if output is high and has changed since last read then 
+
+  if ((digitalRead(OP1)) == HIGH) {
+    if (StateChange == 0) {              // if output is high and has changed since last read then
       Serial.println("Output 1 HIGH");   // print text to serial terminal
       bitSet(Inputs18, 0);               // change know state flag
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 1 LOW");
       bitClear(Inputs18, 0);
-    }  
+    }
   }
 
 
-// Test output 2
+  // OUT 2 testen
   StateChange = bitRead(Inputs18, 1);  // Read last known state of output 2
-  
-if ((digitalRead(OP2)) == HIGH) { 
+
+  if ((digitalRead(OP2)) == HIGH) {
     if (StateChange == 0) {
       Serial.println("Output 2 HIGH");
       bitSet(Inputs18, 1);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 2 LOW");
       bitClear(Inputs18, 1);
-    }  
+    }
   }
 
 
-// Test output 3
+  // OUT 3 testen
   StateChange = bitRead(Inputs18, 2);  // Read last known state of output 3
-  
-if ((digitalRead(OP3)) == HIGH) { 
+
+  if ((digitalRead(OP3)) == HIGH) {
     if (StateChange == 0) {
       Serial.println("Output 3 HIGH");
       bitSet(Inputs18, 2);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 3 LOW");
       bitClear(Inputs18, 2);
-    }  
+    }
   }
 
 
-// Test output 4
-  StateChange = bitRead(Inputs18, 3);    // Read last known state of output 4 
-  
-if ((digitalRead(OP4)) == HIGH) { 
-    if (StateChange == 0) {          
+  // OUT 4 testen
+  StateChange = bitRead(Inputs18, 3);    // Read last known state of output 4
+
+  if ((digitalRead(OP4)) == HIGH) {
+    if (StateChange == 0) {
       Serial.println("Output 4 HIGH");
       bitSet(Inputs18, 3);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 4 LOW");
       bitClear(Inputs18, 3);
-    }  
+    }
   }
-  
-   StateChange = bitRead(Inputs18, 4);    // Read last known state of output 5  
-  
-  if ((digitalRead(OP5)) == HIGH) { 
-    if (StateChange == 0) {          
+
+  StateChange = bitRead(Inputs18, 4);    // Read last known state of output 5
+
+  if ((digitalRead(OP5)) == HIGH) {
+    if (StateChange == 0) {
       Serial.println("Output 5 HIGH");
       bitSet(Inputs18, 4);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 5 LOW");
       bitClear(Inputs18, 4);
-    }  
+    }
   }
-  
-    StateChange = bitRead(Inputs18, 5);    // Read last known state of output 6 
-  
-  if ((digitalRead(OP6)) == HIGH) { 
-    if (StateChange == 0) {          
+
+  StateChange = bitRead(Inputs18, 5);    // Read last known state of output 6
+
+  if ((digitalRead(OP6)) == HIGH) {
+    if (StateChange == 0) {
       Serial.println("Output 6 HIGH");
       bitSet(Inputs18, 5);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 6 LOW");
       bitClear(Inputs18, 5);
-    }  
+    }
   }
-  
-    StateChange = bitRead(Inputs18, 6);    // Read last known state of output 7 
-  
-  if ((digitalRead(OP7)) == HIGH) { 
-    if (StateChange == 0) {          
+
+  StateChange = bitRead(Inputs18, 6);    // Read last known state of output 7
+
+  if ((digitalRead(OP7)) == HIGH) {
+    if (StateChange == 0) {
       Serial.println("Output 7 HIGH");
       bitSet(Inputs18, 6);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 7 LOW");
       bitClear(Inputs18, 6);
-    }  
+    }
   }
-  
-      StateChange = bitRead(Inputs18, 7);    // Read last known state of output 7
-  
-  if ((digitalRead(OP8)) == HIGH) { 
-    if (StateChange == 0) {          
+
+  StateChange = bitRead(Inputs18, 7);    // Read last known state of output 7
+
+  if ((digitalRead(OP8)) == HIGH) {
+    if (StateChange == 0) {
       Serial.println("Output 8 HIGH");
       bitSet(Inputs18, 7);
     }
   }
-else  {
-    if (StateChange == 1){
+  else  {
+    if (StateChange == 1) {
       Serial.println("Output 8 LOW");
       bitClear(Inputs18, 7);
-    }  
+    }
   }
-Serial.println( Inputs18);
+  Serial.println( Inputs18);
 }
--- GAMMA_TX8.ino
+++ GAMMA_TX8.ino
@@ -1,27 +1,27 @@
-/* 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   RF SOLUTIONS SAMPLE GAMMA TRANSMITTER MODE 8 CHANNELS
   WEB SITE : http://www.rfsolutions.co.uk
- 
+
   Address:
   	William Alexander House
 	William Way, Burgess Hill, West Sussex
 	RH15 9AG
 	+44 (0)1444 227900
-	(01444) 227900 
- 
+	(01444) 227900
+
   Code by: AB
   Date:    11/05/2015
-  
+
   SAMPLE CODE
-   
-   toggles channels 1 > 8 with a 3 second delay between each 
-  
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+   toggles channels 1 > 8 with a 3 second delay between each
+
+  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 */
 
-// DEFINE CHANNELS AND OPTION LINKS 
-#define  CH1 2  // All channels are outputs on the Arduino when configured as a transmitter
+// definition von alle Channels
+#define  CH1 2  // Alle Channels sind OUTPUT, wenn sie als Transmitter configuriert sind
 #define  CH2 3
 #define  CH3 4
 #define  CH4 5
@@ -30,80 +30,82 @@
 #define  CH7 8
 #define  CH8 9
 
-#define  OPT1  11 
+#define  OPT1  11
 #define  OPT2  10
 
 void setup() {
-  Serial.begin(19200);         // enable serial to see what button is pressed on a terminal program
+  Serial.begin(19200);         // Serial einschalten auf 19200 baut
 
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);    // all outputs to control GAMMA-868 in TX Mode 
-  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);    // alle OUTPUT für die Transmitter
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);  
+  pinMode(9, OUTPUT);
 
-  digitalWrite(CH1, HIGH);    // defalt start values for all inputs on GAMMA   
+  digitalWrite(CH1, HIGH);    // default Beginn Wert alle Channels
   digitalWrite(CH2, HIGH);
   digitalWrite(CH3, HIGH);
   digitalWrite(CH4, HIGH);
   digitalWrite(CH5, HIGH);
   digitalWrite(CH6, HIGH);
   digitalWrite(CH7, HIGH);
-  digitalWrite(CH8, HIGH); 
+  digitalWrite(CH8, HIGH);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  digitalWrite(CH1,  LOW);
-     delay(1000);
-  digitalWrite(CH1, HIGH);
-     delay(1000);  
-     Serial.println("next channel");
+
+  digitalWrite(CH1,  LOW);            //LED ausgeschaltet
+  delay(1000);                     //eine Pause zwischen HIGH und LOW
+  digitalWrite(CH1, HIGH);            //LED eingeschaltet
+  delay(1000);
+
+  Serial.println("next channel");  // ein Serial Text, wo wird gezeigt, ob zu die nächste channel geht
+
   digitalWrite(CH2,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH2, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
+
   digitalWrite(CH3,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH3, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
   digitalWrite(CH4,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH4, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
   digitalWrite(CH5,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH5, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
   digitalWrite(CH6,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH6, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
   digitalWrite(CH7,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH7, HIGH);
-     delay(3000);
-     
-     Serial.println("next channel");
+  delay(3000);
+
+  Serial.println("next channel");
   digitalWrite(CH8,  LOW);
-     delay(3000);
+  delay(3000);
   digitalWrite(CH8, HIGH);
-     delay(3000);   
+  delay(3000);
 
 }
